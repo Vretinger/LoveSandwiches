d1ad409e835ffbfdb75c176bf34a76861870b3a1
Vretinger Vretinger <brozgamers@hotmail.com>
2024-07-08 15:05:58 +0000

    Wire up APIs and confrim acces to google sheet

--- run.py
+++ run.py
@@ -5,15 +5,15 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
-    CREDS = Credentials.from_service_account_file(`creds.json`)
-    SCOPED_CREDS = CREDS.with scopes(SCOPE)
-    GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-    SHEET = GSPREAD_CLIENT.open(`love_sandwiches`)
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-    sales = SHEET.worksheet(`sales`)
+sales = SHEET.worksheet('sales')
 
-    data = sales.get_all_values()
+data = sales.get_all_values()
 
-    print(data)
+print(data)
