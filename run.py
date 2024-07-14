fe4100f0c6c57dc9d7a0bf160e35b3f0048d8daf
Vretinger Vretinger <brozgamers@hotmail.com>
2024-07-14 13:32:07 +0000

    Collect sales data for the last 5 markets day

--- run.py
+++ run.py
@@ -64,6 +64,17 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)  
+        columns.append(column[-5:])  
+    
+    return columns
+
+
+
 def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
@@ -71,4 +82,5 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
 
-main()
+# main() 
+sales_columns = gitget_last_5_entries_sales()
